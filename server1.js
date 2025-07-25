c3fe85e002274da9ec38b249d7b12c7e0de57f8b
GhazaalSarmadi <ghazal.sar99@gmail.com>
2025-07-25 19:27:53 +0430

    change server1.js

--- server1.js
+++ server1.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
 const products = [
   {
     "id": 1,
